43f3c0a6b250df2c52c69f2f63565c375dfb1df7
rdnasim <rdislamnasim78@gmail.com>
2017-07-14 12:44:03 +0600

    updated

--- app/src/main/java/me/rdnasim/courtcounter/MainActivity.java
+++ app/src/main/java/me/rdnasim/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package me.rdnasim.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,7 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String jamesbond = "hi";
+        String jamesBond = "hello";
+        String s = jamesBond + jamesbond;
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimpSlifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void displayForTeamA(int score) {
         TextView displayScore = (TextView) findViewById(R.id.display_teamA_score);
